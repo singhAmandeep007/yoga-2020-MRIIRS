cd8ae1d49d277d86e65eabc91b012ef24b49cc32
amandeepmicro <55043082+amandeepmicro@users.noreply.github.com>
2020-06-16 10:33:48 +0100

    css+ contact

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.get("/",function(req,res){
 app.get('/home',(req,res)=>{
     res.render('landing.ejs')
 })
+app.get("/contact",(req,res)=>{
+    res.render('contact.ejs')
+})
+app.get("/",(req,res)=>{
+    res.redirect('/home')
+})
 app.listen(process.env.PORT|| 3000,function(){
     console.log("server is up")
 });
